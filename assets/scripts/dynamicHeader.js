d2764e9a2d35977f33255da59eca1f0710059a5d
Joshua Lewis <163526139+joshualewis1717@users.noreply.github.com>
2025-03-29 21:40:52 +0000

    update

--- assets/scripts/dynamicHeader.js
+++ assets/scripts/dynamicHeader.js
@@ -1,10 +1,3 @@
-/*function showHeroText(){
-    const heroText = document.querySelector('#heroText'); 
-
-    heroText.style.transform = "translateY(0)";
-    heroText.style.opacity = '1';
-}*/
-
 function onResize(){
     const headerHeight = window.innerWidth > 768 ? '100px' : '70px';
 
@@ -13,39 +6,29 @@ function onResize(){
     const name = document.querySelector('#companyName');
     const headerImg = document.querySelector('header img:first-child');
 
-    function changeImage(newSrc) {
-        headerImg.style.opacity = 0;
-    
-        setTimeout(() => {
-            headerImg.src = newSrc;
-            headerImg.style.opacity = 1; 
-        }, 300); 
-    }
+    const navSpacer = document.querySelector('#navSpacer');
+    navSpacer.style.height = headerHeight;
+
 
     if (window.scrollY == 0) {                              // If at the top
         header.style.height= headerHeight;
-        header.style.backgroundColor = '#0188BE00';
-        header.style.borderBottom = "1px solid #0188BE00";
         name.style.transform = "translateY(-10px)";
         tel.style.opacity = '1';
         tel.style.visibility = 'visible';
+        headerImg.style.opacity = '1';
+        headerImg.style.pointerEvents = 'auto';
 
-        if (headerImg.src.includes('up')) changeImage('assets/images/menu.svg');
     }  else {                                               // If scrolled down
         header.style.height= '50px';
-        header.style.backgroundColor = '#FFFFFFFF';
-        header.style.borderBottom = "1px solid rgb(122, 122, 122)";
         name.style.transform = "translateY(0)";
         tel.style.opacity = '0';
         tel.style.visibility = 'hidden';
         document.querySelector('nav').style.transform = "translateX(-100%)";
-
-        if (headerImg.src.includes('menu')) changeImage('assets/images/up.svg');
+        headerImg.style.opacity = '0';
+        headerImg.style.pointerEvents = 'none';
     }
 }
 
 window.addEventListener('scroll', onResize);
 window.addEventListener('resize', onResize);
 window.addEventListener('load', onResize);
-
-/*window.addEventListener('load', showHeroText);*/
--- assets/scripts/dynamicNav.js
+++ assets/scripts/dynamicNav.js
@@ -1,14 +1,12 @@
 const btn = document.querySelector('header img:first-child');
 const nav = document.querySelector('nav');
 const close = document.querySelector("#close");
-const header = document.querySelector('header'); 
 
 btn.addEventListener('click', () => {
     if (window.scrollY > 0) {                              // If scrolled down
         window.scrollTo({ top: 0, behavior: 'smooth' });
     }  else {                                               // If at the top
         nav.style.transform = "translateX(0%)";
-
     }
 
 });
